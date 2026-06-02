676af06bac37a521eeaf64c6e8a00228c7c6c59e
trefrog <nwarden@outlook.com>
2026-06-01 19:26:21 -0600

    Windows bugfix

--- src/main.c
+++ src/main.c
@@ -4,16 +4,22 @@
 #include <stdio.h>
 
 int main(void) {
-    App app = {0};
-
-    if (!app_init(&app)) {
-        fprintf(stderr, "app_init failed: %s\n", SDL_GetError());
-        app_shutdown(&app);
+    App *app = (App *)SDL_calloc(1, sizeof(*app));
+    if (!app) {
+        fprintf(stderr, "failed to allocate app state\n");
         return 1;
     }
 
-    app_run(&app);
+    if (!app_init(app)) {
+        fprintf(stderr, "app_init failed: %s\n", SDL_GetError());
+        app_shutdown(app);
+        SDL_free(app);
+        return 1;
+    }
 
-    app_shutdown(&app);
+    app_run(app);
+
+    app_shutdown(app);
+    SDL_free(app);
     return 0;
 }
